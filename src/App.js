f45317e1846da731d4dc810c19124628a728f954
Kees Postma <kk.postma@gmail.com>
2017-10-17 16:07:55 +0100

    Installs Axios

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PassagesContainer from './components/PassagesContainer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Your Sailing Logbook</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <PassagesContainer />
       </div>
     );
   }
---
+++ src/components/PassagesContainer.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class PassagesContainer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      ideas: []
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        Passages
+      </div>
+    )
+  }
+}
+
+export default PassagesContainer
