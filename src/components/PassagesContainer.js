8a6dabc1ad3281f8fe67bd9277aa188ffff870c5
Kees Postma <kk.postma@gmail.com>
2017-10-17 16:33:43 +0100

    Displays the passages on the index page

--- src/components/PassagesContainer.js
+++ src/components/PassagesContainer.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class PassagesContainer extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      ideas: []
+      passages: []
     }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3001/api/v1/passages.json')
+    .then(response => {
+      this.setState({passages: response.data})
+    })
+    .catch(error => console.log(error))
   }
 
   render() {
     return (
       <div>
-        Passages
+        {this.state.passages.map((passage) => {
+          return (
+            <div className="passage" key={passage.id} >
+              <h4>{passage.departure_port}</h4>
+            </div>
+          )
+        })}
       </div>
     )
   }
