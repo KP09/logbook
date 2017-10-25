5bf641e6cd29ebf2fc7a009cce2d85fb0164e34a
Kees Postma <kk.postma@gmail.com>
2017-10-25 20:55:46 +0100

    Installs react-router

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,13 +3,12 @@ import React from 'react';
 export class Menu extends React.Component {
   constructor(props) {
     super(props);
-    this.handleClick = this.handleClick.bind(this);
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.handleClick}>Close</button>
+        <button>Close</button>
         <button>Account Settings</button>
         <button>Passages</button>
         <button>Overview</button>
